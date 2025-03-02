3cf9f2d3b128a48c90f724a225df58d630c9c628
maxim <themain@gmai.com>
2025-03-02 11:11:34 +0300

    !arcface!

--- neon_cicada/main.py
+++ neon_cicada/main.py
@@ -6,13 +6,17 @@ import os
 from PyQt6.QtWidgets import QApplication
 
 # os.environ["QT_QPA_PLATFORM"] = "offscreen"
+encodings_path_arc = 'data/encodings(arcface).pkl'
+labels_path_arc = 'data/train_labels(arcface).pkl'
 encodings_path = 'data/encodings.pkl'
 labels_path = 'data/train_labels.pkl'
 
-face_rec_model = model.FaceModel(encodings_path = encodings_path,
+face_net = model.FaceNetModel(encodings_path = encodings_path,
                                  labels_path = labels_path)
+arc_face = model.ArcFaceModel(encodings_path = encodings_path_arc,
+                                 labels_path = labels_path_arc)
 app = QApplication(sys.argv)
-window = ui.ImageUploaderApp(model = face_rec_model)
+window = ui.ImageUploaderApp(model = arc_face)
 window.show()
 print("Приложение запущено")
 sys.exit(app.exec())
